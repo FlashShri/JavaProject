7e7aa0547d0f37dbb65ea93895147b064c1590f7
FlashShri <shrikantdhengle3101@gmail.com>
2022-11-02 19:44:55 +0530

    "first commit generic"

---
+++ ClassA.java
@@ -0,0 +1,8 @@
+public class ClassA {
+    public static void main(String[] args) {
+        int a=12;
+        int b=32;
+        int c=a+b;
+        System.out.println("addition of a and b ="+c);
+    }
+}
---
+++ demo.java
@@ -0,0 +1,7 @@
+public class demo{
+public static void main(String agrs[]){
+
+    System.out.println("Hi");
+    System.out.println("This is new file"+11);
+}
+} 
