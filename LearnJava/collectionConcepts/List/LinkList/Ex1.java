272a2902a9db728e217efadc66e22031327af724
FlashShri <shrikantdhengle3101@gmail.com>
2022-11-08 13:38:34 +0530

    list

---
+++ LearnJava/collectionConcepts/List/LinkList/Ex1.java
@@ -0,0 +1,17 @@
+import java.util.LinkedList;
+
+class Ex1{
+public static void main(String[] args) {
+    LinkedList  mylist= new LinkedList ();
+
+    mylist.add("Mi");
+    mylist.add("Samsung");
+    mylist.add("Apple");
+
+    System.out.println(mylist);
+
+
+}
+    
+
+}
